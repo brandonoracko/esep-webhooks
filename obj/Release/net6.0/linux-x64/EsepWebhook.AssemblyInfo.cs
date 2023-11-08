ef685e70620fb233fd6a692060bffd3fe4dd1f83
brandonoracko <orackob@hotmail.com>
2023-11-07 21:57:58 -0500

    commit2

--- obj/Release/net6.0/linux-x64/EsepWebhook.AssemblyInfo.cs
+++ obj/Release/net6.0/linux-x64/EsepWebhook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7e60f7a2b4984ada6ba6b500fcca9a7721d67ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EsepWebhook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
